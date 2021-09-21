c6099bfbed889748d7b1a71670e38ae3b787a0f3
Daniel Kish <aox.soljah@gmail.com>
2021-09-21 07:19:16 +0700

    setup categories and product list components. added dummy data array

---
+++ src/DummyData.js
@@ -0,0 +1,174 @@
+
+const cats = [
+    {id: 1, 
+     category_name: 'Animal Products',
+     url: 'https://source.unsplash.com/random/300x300',
+     products: [
+         {
+        product_id: '1', 
+        name: 'Eggs',
+        price_usd: '5.99',
+        description: '12 per pack',
+        seller: '1',
+        img: 'https://source.unsplash.com/random/300x300'
+        },
+        {
+        product_id: '2', 
+        name: 'Milk',
+        price_usd: '4.99',
+        description: '500 ml per bottle',
+        seller: '2',
+        img: 'https://source.unsplash.com/random/300x300'
+        }
+      ]
+  },
+    {id: 2,
+    category_name: 'Beans',
+    url: 'https://source.unsplash.com/random/300x300',
+    products: [
+        {
+       product_id: '3', 
+       name: 'Beans Rosecoco',
+       price_usd: '6.50',
+       description: '2 lbs per bag',
+       seller: '4',
+       img: 'https://source.unsplash.com/random/300x300'
+       },
+       {
+       product_id: '4', 
+       name: 'Dolichos (Njahi)',
+       price_usd: '5.99',
+       description: '0.5 lb per bag',
+       seller: '2',
+       img: 'https://source.unsplash.com/random/300x300'
+       },
+       {
+        product_id: '5', 
+        name: 'Red Beans',
+        price_usd: '6.99',
+        description: '2 lbs per bag',
+        seller: '4',
+        img: 'https://source.unsplash.com/random/300x300'
+        }
+   ]},
+    {id: 3,
+    category_name: 'Cereals',
+    url: 'https://source.unsplash.com/random/300x300',
+    products: [
+        {
+       product_id: '6', 
+       name: 'Maize Bran',
+       price_usd: '9.99',
+       description: '2 lbs per bag',
+       seller: '4',
+       img: 'https://source.unsplash.com/random/300x300'
+       },
+       {
+       product_id: '7', 
+       name: 'Barley',
+       price_usd: '12.99',
+       description: '1 lb per bag',
+       seller: '2',
+       img: 'https://source.unsplash.com/random/300x300'
+       },
+       {
+        product_id: '8', 
+        name: 'Paddy Rice',
+        price_usd: '2.99',
+        description: '2 lbs per bag',
+        seller: '4',
+        img: 'https://source.unsplash.com/random/300x300'
+        }
+   ]},
+    {id: 4,
+    category_name: 'Fruits',
+    url: 'https://source.unsplash.com/random/300x300',
+    products: [
+        {
+       product_id: '9', 
+       name: 'Avocado',
+       price_usd: '2.99',
+       description: '1 avocado per purchase',
+       seller: '1',
+       img: 'https://source.unsplash.com/random/300x300'
+       },
+       {
+       product_id: '10', 
+       name: 'Limes',
+       price_usd: '5.99',
+       description: '2 limes per purchase',
+       seller: '1',
+       img: 'https://source.unsplash.com/random/300x300'
+       }
+   ]},
+    {id: 5, 
+    category_name: 'Peas',
+    url: 'https://source.unsplash.com/random/300x300',
+    products: [
+        {
+       product_id: '11', 
+       name: 'Chic Peas',
+       price_usd: '9.99',
+       description: '1 lb per bag',
+       seller: '4',
+       img: 'https://source.unsplash.com/random/300x300'
+       },
+       {
+       product_id: '12', 
+       name: 'Pidgeon Peas',
+       price_usd: '8.99',
+       description: '1 lb per bag',
+       seller: '4',
+       img: 'https://source.unsplash.com/random/300x300'
+       }
+   ]},
+    {id: 6,
+    category_name: 'Roots & Tubers',
+    url: 'https://source.unsplash.com/random/300x300',
+    products: [
+        {
+       product_id: '13', 
+       name: 'Cassava Chips',
+       price_usd: '7.99',
+       description: '0.5 lb per bag',
+       seller: '4',
+       img: 'https://source.unsplash.com/random/300x300'
+       }
+   ]},
+    {id: 7,
+    category_name: 'Seeds & Nuts',
+    url: 'https://source.unsplash.com/random/300x300',
+    products: [
+       {
+       product_id: '14', 
+       name: 'Groundnuts',
+       price_usd: '6.49',
+       description: '400 grams per bag',
+       seller: '2',
+       img: 'https://source.unsplash.com/random/300x300'
+       }
+   ]},
+    {id: 8,
+    category_name: 'Vegetables',
+    url: 'https://source.unsplash.com/random/300x300',
+    products: [
+       {
+       product_id: '15', 
+       name: 'Carrots',
+       price_usd: '4.49',
+       description: '12 oz per bag',
+       seller: '2',
+       img: 'https://source.unsplash.com/random/300x300'
+       },
+       {
+        product_id: '16', 
+        name: 'Spring Onions',
+        price_usd: '7.99',
+        description: '4 onions per bag',
+        seller: '1',
+        img: 'https://source.unsplash.com/random/300x300'
+        }
+   ]}
+  ]
+
+  export default cats
