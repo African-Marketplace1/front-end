08e0b33d694c001619ad059e64e71261435fb4bd
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-09-23 11:57:45 -0400

    edit user form works

--- src/components/EditUserForm.js
+++ src/components/EditUserForm.js
@@ -1,18 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import chevronLeft from "../assets/chevronLeft.svg";
 import people from "../assets/people.svg";
 import Button from "@mui/material/Button";
+import axios from "axios";
+import { connect } from "react-redux";
+import { setCurrentUser } from "../actions";
+import { CircularProgress } from "@mui/material";
 
 function EditUserForm(props) {
+  const { push } = useHistory();
+  const initialState = {
+    email: props.currentUser.email,
+    img: props.currentUser.img,
+    location: props.currentUser.location,
+    username: props.currentUser.username,
+  };
+  const [formData, setFormData] = useState(initialState);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    console.log(formData);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("submit");
+    setIsFetching(true);
+    axios
+      .put(
+        `https://africanmarketplace-1.herokuapp.com/users/${props.currentUser.user_id}`,
+        formData
+      )
+      .then((res) => {
+        props.setCurrentUser(res.data);
+        setIsFetching(false);
+        push("/user");
+      })
+      .catch((err) => {
+        console.dir(err);
+        setIsFetching(false);
+      });
+
+    console.log(props);
   };
   return (
     <div className="editForm py-5">
       <div className="container">
-        <div className="top">
+        <div className="top mb-4">
           <Link style={{ textDecoration: "none", color: "black" }} to="/user">
             <div className="d-flex align-items-center">
               <div className="pb-1 pe-2">
@@ -21,29 +59,50 @@ function EditUserForm(props) {
               <h5 className="text-start m-0">Back</h5>
             </div>
           </Link>
+          {isFetching && <CircularProgress style={{ position: "absolute" }} />}
         </div>
-        <div className="d-flex">
+        <div className="d-md-flex ">
           <form
             style={{ width: "20rem" }}
             className="m-auto"
             onSubmit={handleSubmit}
           >
-            <h2 className="text-start">Edit User</h2>
+            <h2 className="text-start mb-3">Edit User</h2>
             <label className="d-flex flex-column justify-content-center align-items-start mb-3">
               Username:
-              <input style={{ width: "100%", height: "2.25rem" }} />
+              <input
+                style={{ width: "100%", height: "2.25rem" }}
+                name="username"
+                value={formData.username}
+                onChange={onChange}
+              />
             </label>
             <label className="d-flex flex-column justify-content-center align-items-start mb-3">
               Location:
-              <input style={{ width: "100%", height: "2.25rem" }} />
+              <input
+                style={{ width: "100%", height: "2.25rem" }}
+                name="location"
+                value={formData.location}
+                onChange={onChange}
+              />
             </label>
             <label className="d-flex flex-column justify-content-center align-items-start mb-3">
               Email:
-              <input style={{ width: "100%", height: "2.25rem" }} />
+              <input
+                style={{ width: "100%", height: "2.25rem" }}
+                name="email"
+                value={formData.email}
+                onChange={onChange}
+              />
             </label>
             <label className="d-flex flex-column justify-content-center align-items-start mb-3">
               Profile Image URL:
-              <input style={{ width: "100%", height: "2.25rem" }} />
+              <input
+                style={{ width: "100%", height: "2.25rem" }}
+                name="img"
+                value={formData.img}
+                onChange={onChange}
+              />
             </label>
             <div className="d-flex justify-content-end">
               <Button color="inherit" type="submit">
@@ -52,7 +111,7 @@ function EditUserForm(props) {
             </div>
           </form>
           <div
-            className="imgWrap d-flex justify-content-center align-items-center"
+            className="imgWrap d-flex justify-content-center align-items-center m-auto"
             style={{ width: "40%" }}
           >
             <img
@@ -67,4 +126,10 @@ function EditUserForm(props) {
   );
 }
 
-export default EditUserForm;
+const mapStateToProps = (state) => {
+  return {
+    ...state,
+  };
+};
+
+export default connect(mapStateToProps, { setCurrentUser })(EditUserForm);
